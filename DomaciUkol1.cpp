77a505c8e9ca83496894e94639ff18e2fdc78437
Petr Kuka <kuska.petr@gmail.com>
2015-11-14 15:17:30 +0100

    Add DU 1 - infix to postfix

---
+++ DomaciUkol1.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   DomaciUkol1.cpp.cpp
+ * Author: Petr Kuška
+ *
+ * Created on 11. listopadu 2015, 10:06
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+char P1[] = {'('};              // priorita 1
+char P2[] = {'+', '-'};         // priorita 2
+char P3[] = {'*', '/', '%'};    // priorita 3
+char P4[] = {'~'};              // priorita 4
+
+int main() {
+
+    char input[];
+    cin >> input;
+
+    cout << input << endl;
+
+    return 0;
+}
